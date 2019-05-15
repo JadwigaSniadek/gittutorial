25b85139ee0527a35db72eb21c31120b5e2b8584
JadwigaSniadek <sniadekjadwiga@gmail.com>
2019-05-15 17:17:02 +0200

    4 commit

--- src/pl/agnieszkasniadek/Main.java
+++ src/pl/agnieszkasniadek/Main.java
@@ -3,5 +3,6 @@ package pl.agnieszkasniadek;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Inne zmiany w kodzie");
     }
 }
